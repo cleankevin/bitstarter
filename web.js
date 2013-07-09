7d53577cc7c5be1c8934f916e7fb226ccb188502
cleankevin <haiyuchn@gmail.com>
2013-07-09 13:52:02 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(256);
+buf.write("fs.readFileSync("index.html")","utf-8")
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('buf.toString('utf8')');
 });
 
 var port = process.env.PORT || 5000;
